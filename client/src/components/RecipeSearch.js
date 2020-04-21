8983e067c84074a86cef2aa6bf50d486c6eb9e2a
Andrew English <aje299@gmail.com>
2020-04-20 19:43:36 -0700

    Search box now resizes with the window width

--- client/src/components/RecipeSearch.js
+++ client/src/components/RecipeSearch.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class RecipeSearch extends React.Component {
     constructor(props) {
@@ -8,12 +9,19 @@ class RecipeSearch extends React.Component {
             open: false
         }
     }
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside, true);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
 
     toggleOpen = () => {
         this.setState(prevState => ({ open: !prevState.open }))
     }
 
-    handleSearchChange = (e) => {
+    handleSearchChange = e => {
         let search = e.target.value
 
         // can add search caching in the future for faster searches
@@ -31,18 +39,26 @@ class RecipeSearch extends React.Component {
 
         this.props.updateFilteredRecipes(filteredList)
     }
+
+    handleClickOutside = e => {
+        let domNode = ReactDOM.findDOMNode(this)
+
+        if (!domNode || !domNode.contains(e.target) && this.state.open)
+            this.setState({ open: false })
+    }
     
     render() {
         return (
             <div style={{display: "inline-block"}}>
                 <button
-                    onClick={this.toggleOpen} className="fa fa-search"
+                    onClick={this.toggleOpen} className="recipe-search-icon fa fa-search"
                     style={{background: "none", border: "none", fontSize: "25px", paddingBottom: "20px",
                             paddingTop: "10px", paddingRight: "15px"}}
                 />
                 <input
                     type="search" onChange={(e) => this.handleSearchChange(e)} hidden={!this.state.open}
-                    placeholder="Search..." style={{borderRadius: "10px", border: "1.4px solid", paddingLeft: "5px"}}
+                    placeholder="Search..." style={{borderRadius: "10px", border: "1.4px solid", paddingLeft: "5px",
+                                                    width: "15vw"}}
                 />
             </div>
         )

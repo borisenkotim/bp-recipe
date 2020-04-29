88bb99e9c3c50c61129a1169b68f72dd8f787372
Andrew English <aje299@gmail.com>
2020-04-29 10:25:55 -0700

    Added sorting functionality to sorting buttons

--- client/src/components/RecipeSort.js
+++ client/src/components/RecipeSort.js
@@ -2,24 +2,23 @@ import React from 'react'
 
 class SortBtn extends React.Component {
     constructor(props){
-        super(props);
-        this.state = {
-            asc: true
-        }
+        super(props)
     }
 
+    asc = true
+    
     onClick = () => {
         if (this.props.selected)
-            this.setState(prevState => ({asc: !prevState.asc}))
+            this.asc = !this.asc
 
         this.props.onClick()
-        this.props.sortFunction(this.state.asc)
+        this.props.sortFunction(this.asc)
     }
 
     render() {
         return (
             <button
-                className={`recipe-sort-button ${this.props.icon}${this.state.asc ? "-asc" : "-desc"}`}
+                className={`recipe-sort-button ${this.props.icon}${this.asc ? "-asc" : "-desc"}`}
                 onClick={this.onClick}
             />
         )
@@ -30,12 +29,51 @@ class RecipeSort extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            selectedSort: 0,
+            selectedSort: null,
         }
     }
 
-    nameSort = (asc) => { console.log("in alphasort") }
-    timeSort = (asc) => { console.log("in timesort") }
+    nameSort = (asc) => {
+        let recipes = [...this.props.recipeList]
+        
+        recipes.sort((a, b) =>{
+            let nameA = a.name.toLowerCase();
+            let nameB = b.name.toLowerCase();
+            
+            let result = 0
+            if(nameA < nameB)
+                result = -1
+            if(nameA > nameB)
+                result = 1
+            if(!asc)
+                result *= -1
+            
+            return result
+        })
+
+        this.props.updateFilteredRecipes(recipes)
+    }
+
+    timeSort = (asc) => {
+        let recipes = [...this.props.recipeList]
+        
+        recipes.sort((a, b) => {
+            let timeA = a.cookTime;
+            let timeB = b.cookTime;
+
+            let result = 0
+            if (timeA < timeB)
+                result = -1
+            if (timeA > timeB)
+                result = 1
+            if (!asc)
+                result *= -1
+
+            return result
+        })
+
+        this.props.updateFilteredRecipes(recipes)
+    }
 
     onBtnClick = (btnIndex) => {
         this.setState({selectedSort: btnIndex})

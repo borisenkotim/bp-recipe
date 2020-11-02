870da42ffcdc2de36bf8c278f86d0d3faf3ae03d
Chris Hundhausen <hundhaus@wsu.edu>
2020-11-01 17:06:46 -0800

    Working on deployment Updated react-sortablejs version

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f4d8bb8457e1d07f9fafbaadfb9a1b9.js"
+  "/precache-manifest.6782e03565644005ee0822589292fc06.js"
 );
 
 self.addEventListener('message', (event) => {

bcfb0dae158b2593481709385d0ebf2410e587da
Jacob Plank <jmp0628@yahoo.com>
2020-12-11 00:38:38 -0700

    Added more columns for the pantry table

--- client/src/components/PantryTable.js
+++ client/src/components/PantryTable.js
@@ -16,7 +16,9 @@ class PantryTable extends React.Component {
     table.push(
       <tr id={"tableRow-"+ r}key={r}>
         <td>{this.props.pantry[r].name}</td>
+        <td>{this.props.pantry[r].calories}</td>
     <td>{this.props.pantry[r].quantity}{this.props.pantry[r].unit}</td>
+    <td>{this.props.pantry[r].expiration}</td>
         <td><button id={"button2-"+r}onClick={() => this.editPantry(r)}>
               <span className="fa fa-eye"></span></button></td>
         <td><button id={"button3-"+r}onClick={() => this.confirmDelete(r)}>
@@ -35,7 +37,9 @@ class PantryTable extends React.Component {
         <thead className="thead-light">
         <tr>
           <th>Ingredient</th>
+          <th>Calories</th>
           <th>Quantity</th>
+          <th>Expiration Date</th>
           <th>View/Edit...</th>
           <th>Delete</th>
         </tr>
